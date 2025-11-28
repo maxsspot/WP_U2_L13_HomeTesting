e08ef71a313123c35ffb0698b842f4f85ee7dc06
nope <55818423+maxsspot@users.noreply.github.com>
2025-11-28 16:20:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,26 @@ let twoSelected = [];
 let imageSrcs = ["","","","","","","","","","","","","","","","","","","",""];
 const pic = document.getElementsByClassName("inside_div");
 let hasMatched;
-let p1score=0;
-let p2score=0;
+let p1score;
+let p2score;
 let p1ScoreShown=document.getElementById("p1score");
 let p2ScoreShown=document.getElementById("p2score");
+
+function loadSavedScores() {
+     if(sessionStorage.getItem("p1score")) {
+          p1ScoreShown.textContent = sessionStorage.getItem("p1score");
+          p1score=Number(sessionStorage.getItem("p1score"));
+     } else {
+          p1score=0;
+     }
+     
+     if(sessionStorage.getItem("p2score")) {
+          p2ScoreShown.textContent = sessionStorage.getItem("p2score");
+          p2score=Number(sessionStorage.getItem("p2score"));
+     } else {
+          p2score=0;
+     }
+}
 
 function user_clicks(){
     while(currentPic!=20) {
